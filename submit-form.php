69ee0fea3a4626bebab3b32cd8d8f44203391811
lelnatalia <nnleleko@gmail.com>
2025-03-24 00:57:04 -0700

    form fix

--- submit-form.php
+++ submit-form.php
@@ -1,4 +1,12 @@
 <?php
-echo "message:success";
+$response = array(
+    "status" => "success",
+    "message" => "Data has been processed successfully."
+);
 
+// Set the header to indicate the content type as JSON
+header('Content-Type: application/json');
+
+// Encode the array to JSON and send it as a response
+echo json_encode($response);
 ?>
