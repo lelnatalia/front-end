c5364f7690f0c49a9221581312b34d7d0a9cc631
lelnatalia <nnleleko@gmail.com>
2025-03-29 00:38:38 -0700

    add JMeter report pages

--- submit-form.php
+++ submit-form.php
@@ -1,8 +1,10 @@
 <?php
+$message=$_POST['message'];
 $response = array(
     "status" => "success",
    // "message" => "<b style=color:green">Data has been processed successfully.</b>"
-   "message" => "Data has NOT been processed."
+   "message" => "Data has NOT been processed.",
+   "message_recieved" => ".$message."
 );
 
 // Set the header to indicate the content type as JSON
